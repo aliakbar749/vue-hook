66c132628a0627f7c5f936dbde1a83d5a1d62601
aliakbar749 <aliakbartutul749@gmail.com>
2022-08-18 11:44:07 +0600

    Navabr component add

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,20 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Navbar msg="This is the navbar portion"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/Navbar.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+    Navbar
+}
 }
 </script>
 
